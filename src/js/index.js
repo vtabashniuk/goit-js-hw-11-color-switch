09444e1e87bb1e746bc090708c4061d50c9203b3
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-07-24 15:17:24 +0300

    Добавляет блокировку кнопки Старт

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,13 @@ import randomColor from "./random-color-picker";
 let intervalId = null;
 
 refs.startBtn.addEventListener("click", () => {
+    refs.startBtn.disabled = true;
     intervalId = setInterval(() => {
     refs.bodyElement.style.backgroundColor = randomColor(colors);
   }, 1000);
 });
 
 refs.stopBtn.addEventListener("click", () => {
-  clearInterval(intervalId);
+    clearInterval(intervalId);
+    refs.startBtn.disabled = false;
 });
